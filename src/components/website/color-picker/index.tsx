9442fb53945103dcb7ffa561fc5d36387c2102a1
Nasserkar <nasserk99205@gmail.com>
2025-09-04 17:01:24 +0200

    feat: add color picker component

---
+++ src/components/website/color-picker/index.tsx
@@ -0,0 +1,9 @@
+function ColorPicker() {
+  return (
+    <div className="min-h-40 w-60 bg-black flex flex-col">
+      <div></div>
+    </div>
+  );
+}
+
+export { ColorPicker };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,11 @@
+import { ColorPicker } from "@/components/website/color-picker";
+
 function Home() {
   return (
     <>
-      <section className="min-h-screen w-full"></section>
+      <section className="min-h-screen w-full flex items-center justify-center">
+        <ColorPicker />
+      </section>
     </>
   );
 }
