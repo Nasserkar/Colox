51d587bda04f66a8f2ef84aa144d4ae958dd339e
Nasserkar <nasserk99205@gmail.com>
2025-09-04 17:39:33 +0200

    feat: add slider

--- src/components/website/color-picker/index.tsx
+++ src/components/website/color-picker/index.tsx
@@ -4,13 +4,22 @@ function ColorCanvas() {
       id="color-canvas"
       className="w-full h-50 flex items-center justify-center relative my-4 hue-overlay"
       style={{
-        backgroundColor: "red",
+        backgroundColor: "#ff0000",
       }}
     >
       <div id="light-overlay" className="absolute inset-0 white-overlay">
         <div
           id="black-overlay"
           className="absolute inset-0 black-overlay"
+        ></div>
+      </div>
+      <div id="slider-box" className="relative h-full w-full">
+        <div
+          id="slider"
+          className="slider absolute size-3 box-content border-2 border-white"
+          style={{
+            backgroundColor: "#ffffff",
+          }}
         ></div>
       </div>
     </div>
