52acc8105bb122b6348cc88b919f249842d6f14b
Nasserkar <nasserk99205@gmail.com>
2025-09-04 15:59:20 +0200

    build(config): add Vite configuration and setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  // To load environment variables
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+
+  return {
+    plugins: [react()],
+    server: {
+      // open the browser
+      open: true,
+      // optional: fail if port is taken
+      strictPort: true,
+      port: Number(env.VITE_APP_PORT) || 3000,
+    },
+    preview: {
+      // open the browser
+      open: true,
+      // optional: fail if port is taken
+      strictPort: true,
+      port: Number(env.VITE_APP_PORT) || 3000,
+    },
+    build: {
+      outDir: "build",
+      sourcemap: true,
+    },
+    publicDir: "./public",
+  };
+});
