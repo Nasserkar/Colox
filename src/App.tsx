b6146d6decaffb228c0029ec70ce8245322b4126
Nasserkar <nasserk99205@gmail.com>
2025-09-07 10:47:55 +0200

    feat(route): add Inputs page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import { RootLayout } from "@/layouts";
-import { Home } from "@/pages/home";
+import Home from "@/pages/home";
 import NotFound from "@/pages/not-found";
+import Inputs from "@/pages/inputs";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="inputs" element={<Inputs />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
---
+++ src/pages/inputs.tsx
@@ -0,0 +1,9 @@
+function Inputs() {
+  return (
+    <section className="min-h-screen w-full flex items-center justify-center">
+      <h1>inputs</h1>
+    </section>
+  );
+}
+
+export default Inputs;
