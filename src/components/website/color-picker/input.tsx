6f924224f0d1e86e1d3fddabdfde9db9d7313fbd
Nasserkar <nasserk99205@gmail.com>
2025-09-07 11:36:36 +0200

    style: add selection color

--- src/components/website/color-picker/input.tsx
+++ src/components/website/color-picker/input.tsx
@@ -11,9 +11,9 @@ function NumInput({ className, ...props }: NumberInputProps) {
       autoCapitalize="off"
       data-slot="number-input"
       className={cn(
-        "h-7 px-2 box-content",
+        "h-7 px-2 box-content w-full min-w-0 rounded-none",
         "text-sm text-dark-100 font-medium leading-4 lining-nums tabular-nums",
-        "outline-none border border-dark-100 hover:border-lavender focus-within:border-lavender",
+        "outline-none border border-dark-100 hover:border-lavender focus-within:border-lavender selection:bg-lavender/60",
         className
       )}
       {...props}
--- src/pages/inputs.tsx
+++ src/pages/inputs.tsx
@@ -3,7 +3,7 @@ import { NumInput } from "@/components/website/color-picker/input";
 function Inputs() {
   return (
     <section className="min-h-screen w-full flex items-center justify-center">
-      <NumInput name="size" id="size" />
+      <NumInput name="size" id="size" className="w-20" />
     </section>
   );
 }
